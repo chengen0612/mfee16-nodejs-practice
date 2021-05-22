9d41f3e55d8bb072d6feecece6dc90cee32c38d3
chengen <chengen0612@gmail.com>
2021-05-22 21:08:44 +0800

    fix: changes console to return

--- basic/sum.js
+++ basic/sum.js
@@ -3,11 +3,15 @@ function sum(n) {
     for(let i=0; i<=n; i++) {
         result += i;
     };
-    console.log(result);
+    // console.log(result);
+    return result;
 };
 
-sum(1);
-sum(2);
-sum(5);
-sum(10);
-sum(100000);
+// sum(1);
+// sum(2);
+// sum(5);
+// sum(10);
+// sum(100000);
+console.log(sum(1));
+console.log(sum(5));
+console.log(sum(100000));
