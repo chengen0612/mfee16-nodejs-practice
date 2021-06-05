244244b9d2a5b71241e5fce1d5c7447a5001b4b4
chengen0612 <chengen0612@gmail.com>
2021-06-05 20:57:37 +0800

    update: make my own explanation

--- basic/process.js
+++ basic/process.js
@@ -1,53 +1,51 @@
 // 上課示範的執行程序範例
 
 
-
-// let b = 0;  // --> 1
-
-// // 2 new promise
-// let testPromise = new Promise((resolve, reject) => {
-//   // 3 做加法
-//   let a = 2 + 3;
-//   // 4 印出來
-//   console.log("inside");
-
-//   resolve(a);
-// });
-// // 5 promise 已經準備好了，可以給暗樁了
-
-// // 6 印出 promise obj Promise { 5 }
-// console.log(testPromise);
-// // 7 「設定」排隊回來之後要幹嘛
-// testPromise.then((result) => {
-//   // 因為 11 所以這邊開始執行
-//   b = result;
-//   console.log(b);
-// });
-// // 8 印出來
-// console.log(b);
-// // 9 印出來
-// console.log("outside");
-// // 10. event loop 發現 stack 空，去 queue 搬東西
-// // 11 event loop 搬 jab 回 stack
-
-// =================================
-
-// console.log("Start 1");
-// setTimeout(()=>{
-//   console.log("Timeout 2 "); // --> Libuv
-// }, 0);
-// console.log("End 3");
-
-const fs = require("fs");
-let b = 0; // --> 1
+// 1 宣告b
+let b = 0;
 
 // 2 new promise
+// 6 宣告 testPromise 為 <fulfilled> 狀態的 Promise { 5 } 物件
 let testPromise = new Promise((resolve, reject) => {
   // 3 做加法
   let a = 2 + 3;
   // 4 印出來
   console.log("inside");
-  // 5 「開始」讀檔案
+  // 5 設定promise為fulfilled狀態且回傳值為a
+  resolve(a);
+});
+
+// 7 印出 promise obj Promise { 5 }
+console.log(testPromise);
+// 8 testPromise已完成，then()裡的函式進queue排隊準備異步調用
+testPromise.then((result) => {
+  // 因為 12 所以這邊開始執行
+  b = result;
+  console.log(b);
+});
+// 9 印出來
+console.log(b);
+// 10 印出來
+console.log("outside");
+// 11 event loop 發現 stack 空，去 queue 搬東西
+// 12 event loop 搬 job 回 stack
+
+// =================================
+
+
+const fs = require("fs");
+
+// 1 宣告b
+let b = 0;
+
+// 2 new promise
+// 6 宣告testPromise 為 <pending> 狀態的 promise 物件
+let testPromise = new Promise((resolve, reject) => {
+  // 3 做加法
+  let a = 2 + 3;
+  // 4 印出來
+  console.log("inside");
+  // 5 非同步函式進queue，Promise沒有得到結果，狀態為pending
   fs.readFile("quiz.md", (err, result) => {
     console.log("讀完檔案");
     if (err) {
@@ -56,13 +54,12 @@ let testPromise = new Promise((resolve, reject) => {
     resolve(result);
   });
 });
-//  6 promise 已經準備好了，可以給暗樁了
 
 // 7 印出 promise obj Promise { pending }
 console.log(testPromise);
-// 8 「設定」排隊回來之後要幹嘛
+// 8 設定fs.readFile()從queue返回結果後做什麼事
 testPromise.then((result) => {
-  // 因為 93
+  // 因為 92 開始執行
   b = result;
   console.log("then");
 });
@@ -71,11 +68,10 @@ console.log(b);
 // 10 印出來
 console.log("outside");
 
-// 11 Event loop 就會去看 Q，因為檔案未讀完，所以 Q 空，沒東西搬
-// 12 Event loop 就會去看 Q，因為檔案未讀完，所以 Q 空，沒東西搬
+// 11 Event loop 去看 Q，因為檔案未讀完，所以 Q 空，沒東西搬
+// 12 Event loop 去看 Q，因為檔案未讀完，所以 Q 空，沒東西搬
 // ....
-// 89 Event loop 就會去看 Q，因為檔案讀完，所以 Q 有東西了 --> readFile 的 cb
-// 90 cb --> stack
-// 91 因為成功，所以在 cb 裡呼叫了 resovle <-- promise
-// 92 resovle -> micro queue
-// 93 因為 stack 又空了，Event loop 就會去看 Q ，有 resovle -> 丟回 stack
+// 89 Event loop 去看 Q，因為檔案讀完，所以 Q 有東西了 ， 
+// 90 把 readFile 的 callback 搬回 stack 執行， Promise 被定義為 <fulfilled> 狀態並獲得回傳值
+// 91 Promise 被實現後執行 then() 方法異部調用當中的函式
+// 92 因為 stack 又空了，Event loop 去看 Q ，把 then() 丟出去的函式調用回來
