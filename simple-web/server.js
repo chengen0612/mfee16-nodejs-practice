12ca5ab4dd6a6e4bac98af18c4dd4d81a417fefd
chengen0612 <chengen0612@gmail.com>
2021-06-07 17:03:17 +0800

    udpate: change notes

--- simple-web/server.js
+++ simple-web/server.js
@@ -1,41 +1,38 @@
-// nodejs 內建套件，不用外連
-const http = require("http");
+// built-in modules below, no need to install
+const http = require('http');
+const { URL } = require('url');
+const fs = require('fs/promises');
 
-// createServer(Listener)
-// Listener(request, respone) 
-// request 是請求物件
-// response 是回覆物件
-// 試著console.log()看看req裡面有什麼
+// set up server maker
+// check out what's inside the request obj with console
 const server = http.createServer((req, res) => {
-    console.log("收到連線請求");
-    console.log(req.url); // 查看請求連線
+    // handle incoming request here
+    console.log('收到連線請求...');
+    console.log('請求連線到網址:', req.url); // url client trying to access
 
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain;charset = UTF-8");
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
 
     switch (req.url) {
-        case "/" :
-            res.end("這是首頁");
+        case '/' :
+            res.end('這是首頁');
             break;
-        case "/test" :
-            res.end("這是測試頁");
+        case '/test' :
+            res.end('這是測試頁');
             break;
-        case "/about" :
-            res.end("這是關於我們");
+        case '/about' :
+            res.end('這是關於我們');
             break;
+        // if not exist
         default:
             res.writeHead(404);
-            res.end("查無此網頁");
+            res.end('查無此網頁');
     };
 });
 
-
-// server.listen(port, callback function)
-// port 網路協定
+// listen event
+// only recieve request to 3000 port.
+// if request had made, run executor(callback) set by http.createServer
 server.listen(3000, () => {
-    console.log("設置連線監聽事件: 接收3000port的請求")
-})
-
-const connection = mysql.createConnection();
-
-connection.connect
+    console.log("設置監聽事件: 接收向 3000 port 發出的連線請求")
+});
