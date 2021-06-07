18a80e44be311be4b35845cff75f768818d02695
chengen0612 <chengen0612@gmail.com>
2021-06-08 04:33:26 +0800

    refactor: use if else in a smart way

--- crawler/mysql.js
+++ crawler/mysql.js
@@ -14,7 +14,7 @@ const today = dayjs().format('YYYYMMDD');
 // create connection
 let connection = mysql.createConnection({
 	host     : process.env.DB_HOST,
-  port     : process.env.DB_PORT,
+  	port     : process.env.DB_PORT,
 	user     : process.env.DB_USER,
 	password : process.env.DB_PASSWORD,
 	database : process.env.DB_NAME,
@@ -29,30 +29,35 @@ connection = Promise.promisifyAll(connection);
 (async function() {
 	try {
 		await connection.connectAsync();
+
 		let stockCode = await fs.readFile("stock.txt", "utf8");
-		// verification
-		let ver = await connection.queryAsync(`SELECT stock_id FROM stock WHERE stock_id = '${stockCode}'`);
-		if (ver.length === 0) {
-			let response = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
-			if (response.status === 200){
-				// console.log('query success');
-				let target = response.data.suggestions
-					.map((item) => {
-						return item.split('\t');
-					})
-					.find((item) => {
-						return item[0] === stockCode;
-					});
-				// insert
-				await connection.queryAsync(`INSERT INTO stock (stock_id, stock_name) VALUES ('${target[0]}', '${target[1]}')`);
-        console.log('insert success');
-			};
-		} else {
-			console.log('該筆資料已存在');
-		};
+
+		// repeat?
+		let num = await connection.queryAsync('SELECT stock_id FROM stock WHERE stock_id = ?', [stockCode]);
+		if (num.length > 0) throw '該筆資料已存在';
+
+		// exist?
+		let response = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
+		let symbol = response.data.suggestions[0];
+		if (symbol === '(無符合之代碼或名稱)') throw '查無符合資料';
+
+		// query success
+		let target = response.data.suggestions
+			.map((item) => {
+				return item.split('\t');
+			})
+			.find((item) => {
+				return item[0] === stockCode;
+			});
+
+		// insert success
+		await connection.queryAsync(`INSERT INTO stock (stock_id, stock_name) VALUES ('${target[0]}', '${target[1]}')`);
+
 	} catch (err) {
+		console.log('error');
 		console.error(err);
 	} finally {
+		console.log('finally')
 		connection.endAsync();
 	};
 })();
