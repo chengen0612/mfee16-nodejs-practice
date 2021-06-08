3c4cd09148e89bb77489129c00627949af1fcb59
chengen0612 <chengen0612@gmail.com>
2021-06-08 08:27:44 +0800

    develop: try new stuff

--- crawler/mysql.js
+++ crawler/mysql.js
@@ -30,28 +30,61 @@ connection = Promise.promisifyAll(connection);
 	try {
 		await connection.connectAsync();
 
-		let stockCode = await fs.readFile("stock.txt", "utf8");
+		// let stockCode = await fs.readFile("stock.txt", "utf8");
 
-		// repeat?
-		let num = await connection.queryAsync('SELECT stock_id FROM stock WHERE stock_id = ?', [stockCode]);
-		if (num.length > 0) throw '該筆資料已存在';
+		// // repeat?
+		// let num = await connection.queryAsync('SELECT stock_id FROM stock WHERE stock_id = ?', [stockCode]);
+		// if (num.length > 0) throw '該筆資料已存在';
 
-		// exist?
-		let response = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
-		let symbol = response.data.suggestions[0];
-		if (symbol === '(無符合之代碼或名稱)') throw '查無符合資料';
+		// // exist?
+		// let response = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`);
+		// let symbol = response.data.suggestions[0];
+		// if (symbol === '(無符合之代碼或名稱)') throw '無符合之代碼或名稱';
 
-		// query success
-		let target = response.data.suggestions
-			.map((item) => {
-				return item.split('\t');
-			})
-			.find((item) => {
-				return item[0] === stockCode;
+		// // query success
+		// let target = response.data.suggestions
+		// 	.map((item) => {
+		// 		return item.split('\t');
+		// 	})
+		// 	.find((item) => {
+		// 		return item[0] === stockCode;
+		// 	});
+
+		// // insert success
+		// await connection.queryAsync('INSERT INTO stock (stock_id, stock_name) VALUES (?, ?)', target);
+
+		// query();
+
+		let queryArr = await connection.queryAsync('SELECT stock_id FROM stock');
+		// let stock = queryArr[0]['stock_id'];
+		// 
+		// let totalSt = queryArr.length;
+		// let queryRes = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?', {
+		// 	params : {
+				// response: JSON,
+				// date: today,
+				// stockNo: stock
+		// 	}
+		// });
+		// console.log(queryRes.data.fields);
+		// console.log(queryRes.data.data[0]);
+		let result = queryArr.map(async (stocks) => {
+			// return stocks['stock_id'];
+			let stock = stocks['stock_id'];
+			let data = await axios({
+				method: 'get',
+				url: 'https://www.twse.com.tw/exchangeReport/STOCK_DAY',
+				params: {
+					response: JSON,
+					date: today,
+					stockNo: stock
+				}
 			});
-
-		// insert success
-		await connection.queryAsync(`INSERT INTO stock (stock_id, stock_name) VALUES ('${target[0]}', '${target[1]}')`);
+			// 處理資料
+			// insert data
+			if (data.data.data !== undefined) console.log(data.data.data);
+		});
+		
 
 	} catch (err) {
 		console.log('error');
@@ -61,3 +94,7 @@ connection = Promise.promisifyAll(connection);
 		connection.endAsync();
 	};
 })();
+
+async function query() {
+	console.log('jump to query');
+};
